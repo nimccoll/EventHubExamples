df9fde22a565db2bb91de05431e645b8b31961d3
Nicholas McCollum <nimccoll@microsoft.com>
2020-04-13 11:04:20 -0400

    Initial check-in

---
+++ EventHubCheckPoint/CheckPointEventProcessor.cs
@@ -0,0 +1,46 @@
+﻿//===============================================================================
+// Microsoft FastTrack for Azure
+// Event Hub Checkpoint Sample
+//===============================================================================
+// Copyright © Microsoft Corporation.  All rights reserved.
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+// OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE.
+//===============================================================================
+using Microsoft.ServiceBus.Messaging;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventHubCheckPoint
+{
+    public class CheckPointEventProcessor : IEventProcessor
+    {
+        public void Dispose()
+        {
+        }
+
+        public async Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            Trace.TraceInformation("CheckPointEventProcessor Shutting Down. Partition '{0}', Reason: '{1}'.", context.Lease.PartitionId, reason);
+            if (reason == CloseReason.Shutdown)
+            {
+                await context.CheckpointAsync();
+            }
+        }
+
+        public Task OpenAsync(PartitionContext context)
+        {
+            Trace.TraceInformation("CheckPointEventProcessor initialized.  Partition: '{0}', Offset: '{1}'", context.Lease.PartitionId, context.Lease.Offset);
+            return Task.FromResult<object>(null);
+        }
+
+        public async Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            Trace.TraceInformation("CheckPointEventProcessor processing {0} messages from partition {1}...", messages.ToList().Count, context.Lease.PartitionId);
+            await context.CheckpointAsync();
+        }
+    }
+}
---
+++ EventHubCheckPoint/Program.cs
@@ -0,0 +1,35 @@
+﻿//===============================================================================
+// Microsoft FastTrack for Azure
+// Event Hub Checkpoint Sample
+//===============================================================================
+// Copyright © Microsoft Corporation.  All rights reserved.
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+// OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE.
+//===============================================================================
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Configuration;
+
+namespace EventHubCheckPoint
+{
+    class Program
+    {
+        static readonly string _eventHubName = ConfigurationManager.AppSettings["EventHubName"];
+        static readonly string _consumerGroupName = ConfigurationManager.AppSettings["ConsumerGroupName"];
+        static readonly string _eventHubConnectionString = ConfigurationManager.AppSettings["EventHubConnectionString"];
+        static readonly string _storageConnectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+
+        static void Main(string[] args)
+        {
+            EventProcessorOptions options = new EventProcessorOptions();
+            options.ExceptionReceived += (sender, e) => { Console.WriteLine(e.Exception); };
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(Guid.NewGuid().ToString(), _eventHubName, _consumerGroupName, _eventHubConnectionString, _storageConnectionString);
+            eventProcessorHost.RegisterEventProcessorAsync<CheckPointEventProcessor>(options).Wait();
+            Console.WriteLine("*** Receiving Events. Press any key to stop.");
+            Console.ReadLine();
+            eventProcessorHost.UnregisterEventProcessorAsync();
+        }
+    }
+}
